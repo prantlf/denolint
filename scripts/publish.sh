7065c2466f62733369e364bd3edc007e4acc6d97
Ferdinand Prantl <prantlf@gmail.com>
2022-09-29 04:59:36 +0200

    fix: Fix incomplete publishing of native packages

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -1,18 +1,18 @@
 #!/bin/sh
 
-set -e
+set +e
 set -x
 
 pnpm -r publish --no-git-checks
 
 for dir in packages/denolint/npm/*; do
-  cd packages/denolint/npm/win32-x64-msvc
+  cd $dir
   pnpm publish --no-git-checks
   cd ../../../..
 done
 
 for dir in packages/libdenolint/npm/*; do
-  cd packages/libdenolint/npm/win32-x64-msvc
+  cd $dir
   pnpm publish --no-git-checks
   cd ../../../..
 done
