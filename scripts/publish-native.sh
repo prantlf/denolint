fb13c615d9cd499c67527d4b3bd644d58b30f724
Ferdinand Prantl <prantlf@gmail.com>
2022-10-09 09:03:05 +0200

    fix: Remove publishing settings from package.json
    It causes login prompt if NPM proxy is configured.

--- scripts/publish-native.sh
+++ scripts/publish-native.sh
@@ -5,18 +5,20 @@ set -x
 
 pkg="$1"
 
+publish() {
+  cd $1
+  pnpm publish --no-git-checks --access public
+  cd ../../../..
+}
+
 if [ "$pkg" = "both" ] || [ "$pkg" = "denolint" ]; then
   for dir in packages/denolint/npm/*; do
-    cd $dir
-    pnpm publish --no-git-checks
-    cd ../../../..
+    publish $dir
   done
 fi
 
 if [ "$pkg" = "both" ] || [ "$pkg" = "libdenolint" ]; then
   for dir in packages/libdenolint/npm/*; do
-    cd $dir
-    pnpm publish --no-git-checks
-    cd ../../../..
+    publish $dir
   done
 fi
