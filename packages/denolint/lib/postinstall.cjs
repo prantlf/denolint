8402d71eea162343cf38ae6cd76029ecf2b8f152
Ferdinand Prantl <prantlf@gmail.com>
2022-10-16 10:57:30 +0200

    fix: Fix installing bin script on Windows

--- packages/denolint/lib/postinstall.cjs
+++ packages/denolint/lib/postinstall.cjs
@@ -1,4 +1,4 @@
-const { chmodSync, existsSync, lstatSync, mkdirSync, readFileSync, symlinkSync, unlinkSync } = require('fs')
+const { chmodSync, lstatSync, mkdirSync, readFileSync, symlinkSync, unlinkSync, writeFileSync } = require('fs')
 const { basename, join } = require('path')
 
 const { platform, arch, env } = process
@@ -42,29 +42,62 @@ if (optionalDependencies) {
   const suffix = archs[arch]
   if (!suffix) throw new Error(`Unsupported architecture: ${arch}`)
   log && console.log(`[denolint] Platform ${platform}, architecture ${arch}, suffix ${suffix()}.`)
-  const target = require.resolve(`@denolint/denolint-${platform}-${suffix()}`)
-  if (platform !== 'win32') {
-    log && console.log(`[denolint] Making "${target}" executable.`)
-    chmodSync(target, 0o755)
-  }
+  const platDir = `denolint-${platform}-${suffix()}`
   const binDir = join(`${env.INIT_CWD}/node_modules`, '.bin')
   log && console.log(`[denolint] Creating "${binDir}".`)
   mkdirSync(binDir, { recursive: true })
-  const link = join(binDir, basename(target))
-  const stat = lstatSync(link, { throwIfNoEntry: false })
-  if (!stat) {
-    log && console.log(`[denolint] Linking "${target}" to "${link}".`)
-    symlinkSync(target, link, 'junction')
-  } else {
-    if (!existsSync(link)) {
-      log && console.log(`[denolint] Removing invalid "${link}".`)
-      unlinkSync(link)
-      log && console.log(`[denolint] Linking "${target}" to "${link}".`)
-      symlinkSync(target, link, 'junction')
-    } else {
-      log && console.log(`[denolint] "${link}" already exists.`)
-    }
-  }
+  if (platform !== 'win32') installBin(platDir, binDir)
+  else installExe(platDir, binDir)
 } else {
   log && console.log('[denolint] Development installation detected.')
+}
+
+function installBin(platDir, binDir) {
+  const target = require.resolve(`@denolint/${platDir}`)
+  log && console.log(`[denolint] Making "${target}" executable.`)
+  chmodSync(target, 0o755)
+  const link = join(binDir, basename(target))
+  const stat = lstatSync(link, { throwIfNoEntry: false })
+  if (stat) {
+    log && console.log(`[denolint] Removing previous "${link}".`)
+    unlinkSync(link)
+  }
+  log && console.log(`[denolint] Creating "${link}".`)
+  symlinkSync(target, link)
+}
+
+function installExe(platDir, binDir) {
+  const sh = `#!/bin/sh
+basedir=$(dirname "$(echo "$0" | sed -e 's,\\\\,/,g')")
+exec "$basedir/../@denolint/${platDir}/denolint.exe" "$@"
+`
+  const cmd = `@ECHO off
+GOTO start
+:find_dp0
+SET dp0=%~dp0
+EXIT /b
+:start
+SETLOCAL
+CALL :find_dp0
+ENDLOCAL & "%dp0%\\..\\@denolint\\${platDir}\\denolint.exe" %*
+`
+  const ps1 = `#!/usr/bin/env pwsh
+$basedir=Split-Path $MyInvocation.MyCommand.Definition -Parent
+if ($MyInvocation.ExpectingInput) {
+  $input | & "$basedir/../@denolint/${platDir}/denolint.exe" $args
+} else {
+  & "$basedir/../@denolint/${platDir}/denolint.exe" $args
+}
+exit $LASTEXITCODE
+`
+  const base = join(binDir, 'denolint')
+  writeScript('', sh, { mode: 0o755 })
+  writeScript('.cmd', cmd)
+  writeScript('.ps1', ps1)
+
+  function writeScript(ext, text, options) {
+    const script = `${base}${ext}`
+    log && console.log(`[denolint] Creating "${script}".`)
+    writeFileSync(script, text, options)
+  }
 }
